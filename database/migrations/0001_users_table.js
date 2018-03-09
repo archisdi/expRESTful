c6052f08597ade40880de553ab17ca078726190c
Archie <archie.isdiningrat@bizzy.co.id>
2018-03-10 00:51:38 +0700

    improve migration

---
+++ database/migrations/0001_users_table.js
@@ -0,0 +1,43 @@
+module.exports = {
+    up: function (queryInterface, DataTypes) {
+        return queryInterface.createTable('users', {
+            id: {
+                type: DataTypes.INTEGER(10).UNSIGNED,
+                allowNull: false,
+                primaryKey: true,
+                autoIncrement: true,
+                field: 'id'
+            },
+            name: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                field: 'name'
+            },
+            username: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                unique: true,
+                field: 'username'
+            },
+            password: {
+                type: DataTypes.STRING(255),
+                allowNull: false,
+                field: 'password'
+            },
+            createdAt: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: 'created_at'
+            },
+            updatedAt: {
+                type: DataTypes.DATE,
+                allowNull: true,
+                field: 'updated_at'
+            }
+        });
+    },
+
+    down: function (queryInterface, Sequelize) {
+        return queryInterface.dropTable('users');
+    }
+};

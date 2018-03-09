bfec5033a90c59c4844931efdce78a68c6221cd0
Archie <archie.isdiningrat@bizzy.co.id>
2018-03-09 19:41:45 +0700

    add helmet

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const bodyParser = require('body-parser');
 const ErrorException = require('./app/exceptions/error_exception');
 const NotFoundException = require('./app/exceptions/not_found_exception');
 const ApiGuard = require('./app/middleware/api_guard');
-const RateLimiter = require('./app/helpers/rate_limiter')();
+const RateLimiter = require('./app/helpers/rate_limiter');
+const Helmet = require('helmet')
 
 const routes = require('./routes');
 const app = express();
 
 // Golbal Middleware
+app.use(Helmet());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(ApiGuard);
-app.use(RateLimiter);
+app.use(RateLimiter());
 
 routes(app);
 

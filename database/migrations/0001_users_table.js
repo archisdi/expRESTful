438da5987a22ffc6b5874495c3f37fb950022909
Archie <archie.isdiningrat@bizzy.co.id>
2018-05-04 20:14:33 +0700

    abstract model

--- database/migrations/0001_users_table.js
+++ database/migrations/0001_users_table.js
@@ -1,58 +1,6 @@
+const UserSchema = require('../../app/models/sql/scheme/user_scheme');
+
 module.exports = {
-    up: (queryInterface, DataTypes) => {
-        const schema = queryInterface.createTable('users', {
-            id: {
-                type: DataTypes.INTEGER(10).UNSIGNED,
-                allowNull: false,
-                primaryKey: true,
-                autoIncrement: true,
-                field: 'id'
-            },
-            uuid: {
-                type: DataTypes.UUID,
-                field: 'uuid',
-                defaultValue: DataTypes.UUIDV4
-            },
-            name: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-                field: 'name'
-            },
-            username: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-                unique: true,
-                field: 'username'
-            },
-            password: {
-                type: DataTypes.STRING(255),
-                allowNull: false,
-                field: 'password'
-            },
-            refreshToken: {
-                type: DataTypes.STRING(255),
-                allowNull: true,
-                field: 'refresh_token'
-            },
-            tokenValidity: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                field: 'token_validity'
-            },
-            createdAt: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                field: 'created_at'
-            },
-            updatedAt: {
-                type: DataTypes.DATE,
-                allowNull: true,
-                field: 'updated_at'
-            }
-        });
-
-        return schema;
-    },
-
+    up: (queryInterface, DataTypes) => queryInterface.createTable('users', UserSchema(DataTypes)),
     down: (queryInterface, Sequelize) => queryInterface.dropTable('users')
 };

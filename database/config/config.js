3bcb1cbb8061ca6ccadc4e748075a05610f8a240
Archie <archie.isdiningrat@bizzy.co.id>
2018-06-23 10:37:43 +0700

    change migration config

--- database/config/config.js
+++ database/config/config.js
@@ -5,6 +5,7 @@ module.exports = {
         username: process.env.DB_USERNAME,
         password: process.env.DB_PASSWORD,
         database: process.env.DB_NAME,
+        port: process.env.DB_PORT,
         host: process.env.DB_HOST,
         dialect: process.env.DB_DIALECT
     }

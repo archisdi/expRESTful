24be394ea0739901a2e8ae3e98387785e0c01a4e
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-02-21 22:32:07 +0700

    improve migration script

--- scripts/migrations/migrate.sh
+++ scripts/migrations/migrate.sh
@@ -6,6 +6,6 @@ sequelize db:migrate
 echo "\n ${green}Migrating Success"
 
 if [[ $1 == "seed" ]]; then
-        sequelize db:seed:all
+        ../../node_modules/.bin/sequelize db:seed:all
         echo "\n ${green}Seeding Success"
 fi

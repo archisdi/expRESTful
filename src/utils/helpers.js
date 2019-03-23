08c84ea5e46e0a17d515e21bcd97189f324ec514
Archie Isdiningrat <archie.isdiningrat@gmail.com>
2019-03-23 20:30:30 +0700

    add method in helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,4 +4,8 @@ exports.parseDataObject = object => JSON.parse(JSON.stringify(object));
 
 exports.offset = (page = 1, limit = 10) => ((page - 1) * limit);
 
+exports.isEmptyObject = object => !Object.keys(object).length;
+
+exports.isEmptyArray = array => array.length === 0;
+
 module.exports = exports;
